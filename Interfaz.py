68b7c534a59a9bbfe0f15b0d106af18390fc0c8f
InigoUrruchi <iug1002@alu.ubu.es>
2024-10-22 11:59:29 +0200

    funcion actualizar atributo

--- Interfaz.py
+++ Interfaz.py
@@ -10,28 +10,45 @@ def cargar_configuracion():
     configurador.title("configurar")
     configurador.geometry("1000x1000")
 
-    button3 = customtkinter.CTkButton(configurador, text="Aceptar", command=pulsar_boton)
-    button3.grid(row=0, column=7, padx=20, pady=20)
-
-    radio1 = customtkinter.CTkEntry(configurador, placeholder_text=f"Radio actual = {buscar_atributo('geom', 'ball1', 'size')}", )
+    radio1 = customtkinter.CTkEntry(configurador, placeholder_text="Radio de la bola 1", )
     radio1.grid(row=0, column=1, padx=20, pady=20)
+    
+    #Obtener el nuevo valor del radio de bola 1
+    radio_actual = customtkinter.CTkLabel(configurador, text=f"Radio actual = {buscar_atributo('geom', 'ball1', 'size')}")
+    radio_actual.grid(row=0, column=0, padx=20, pady=20)
 
-    #radio_actual = customtkinter.CTkLabel(configurador, text=f"Radio actual = {buscar_atributo('geom', 'ball1', 'size')}")
-   # radio_actual.grid(row=0, column=0, padx=20, pady=20)
+    #Actualizar el valor del radio de la bola 1 
+    button3 = customtkinter.CTkButton(configurador, text="Aceptar", command=lambda: actualizar_atributo('geom', 'ball1', 'size', radio1.get()))
+    button3.grid(row=0, column=7, padx=20, pady=20)
 
     configurador.mainloop()
 
+#Iniciar el simulador
 def simular():
     inicializador = simulador("escenario//escena.xml")
     inicializador.run()
 
-def buscar_atributo(objeto, nombre, atributo):
+#Busca el valor del atributo del objeto que se quiera y lo devuelve como str
+def buscar_atributo(objeto, nombre, atributo,):
     tree= ET.parse("escenario//escena.xml")
     root = tree.getroot()
     for elem in root.findall(f".//{objeto}"):
         if elem.get('name') == nombre:
-            radio = elem.get(atributo)
-            return str(radio)
+            valor_atributo = elem.get(atributo)
+            return str(valor_atributo)
+
+
+#Actualiza en el xml el valor del atributo del objeto que se quiera 
+def actualizar_atributo(objeto, nombre, atributo, valor_atributo):
+    tree= ET.parse("escenario//escena.xml")
+    root = tree.getroot()
+    print(f"El nuebo valor del atributo es {valor_atributo}")
+    for elem in root.findall(f".//{objeto}"):
+        if elem.get('name') == nombre:
+            elem.set(atributo, valor_atributo)
+    tree.write("escenario//escena.xml")
+    print(f"El nuevo valor de {atributo} es {valor_atributo}")
+
 
 app = customtkinter.CTk()
 app.title("my app")
